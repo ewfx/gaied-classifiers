43d2276f890ce922ec1c9b73136acfdbd4b2ce0b
VarunBansal126 <varun.bansal126@gmail.com>
2025-03-25 15:33:53 +0530

    Update config.py

--- code/src/config.py
+++ code/src/config.py
@@ -1,3 +1,3 @@
-tesseract_cmd_path = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-apiKey = "YourGeminiGenAIKey"
+tesseract_cmd_path = r"C:\Program Files\Tesseract-OCR\tesseract.exe" #your_tesseract_path_here
+apiKey = "your_genai_api_key_here" #your_genai_api_key_here
 GenAIModelName = "gemini-2.0-flash"
